20b24f5b796a42c938239f227740af42f591b0cf
nehru00123@gmail.com <nehru00123@gmail.com>
2025-06-22 19:48:55 +0530

    new environments are added

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ionic.dats',
-  appName: 'dats',
+  appId: 'io.ionic.starter',
+  appName: 'ionic-app-base',
   webDir: 'www'
 };
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  AuthUrl: 'https://dats-backend-20112756662.asia-south1.run.app/',
+};
