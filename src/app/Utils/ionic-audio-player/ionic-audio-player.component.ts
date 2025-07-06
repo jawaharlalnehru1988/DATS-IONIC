9172011f0165843f85f51d4843b6bbd4b6aa9a2f
jawaharlalnehru1988 <nehru00123@gmail.com>
2025-07-06 17:20:54 +0530

    audio library is ready

--- src/app/Utils/ionic-audio-player/ionic-audio-player.component.ts
+++ src/app/Utils/ionic-audio-player/ionic-audio-player.component.ts
@@ -1,16 +1,18 @@
 import { IonCardHeader, IonCardTitle, IonCard, IonCardSubtitle, IonCardContent, IonButton, IonIcon, IonRange } from '@ionic/angular/standalone';
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { addIcons } from 'ionicons';
 import { pause, play, repeat, stop, stopwatchSharp, volumeHigh, volumeLow } from 'ionicons/icons';
+import { DatePipe } from '@angular/common';
+
 @Component({
   selector: 'app-ionic-audio-player',
   templateUrl: './ionic-audio-player.component.html',
   styleUrls: ['./ionic-audio-player.component.scss'],
-  imports: [IonCardHeader, IonCardTitle, IonCard, IonCardSubtitle, IonCardContent, IonButton, IonIcon, IonRange ]
+  imports: [IonCardHeader, DatePipe, IonCardTitle, IonCard, IonCardSubtitle, IonCardContent, IonButton, IonIcon, IonRange ]
 })
 export class IonicAudioPlayerComponent  {
 
-  constructor(){
+  constructor(private cdr: ChangeDetectorRef){
     addIcons( {repeat, play, pause, stop, volumeHigh, volumeLow })
   }
 
@@ -26,6 +28,8 @@ export class IonicAudioPlayerComponent  {
   isLoop = false;
   volume = 1;
 
+  duration = 0;
+  
   play() {
     this.audioElement.nativeElement.play();
     this.isPlaying = true;
@@ -41,14 +45,38 @@ export class IonicAudioPlayerComponent  {
     this.audioElement.nativeElement.currentTime = 0;
     this.isPlaying = false;
   }
-
+  
   toggleLoop() {
     this.isLoop = !this.isLoop;
     this.audioElement.nativeElement.loop = this.isLoop;
   }
-
+  
   onVolumeChange(event: any) {
     this.volume = event.detail.value;
     this.audioElement.nativeElement.volume = this.volume;
   }
+  
+  currentTime = 0;
+    onTimeUpdate() {
+    this.currentTime = this.audioElement.nativeElement.currentTime;
+      this.cdr.detectChanges(); 
+  }
+
+formatTime(seconds: number): string {
+  if (!seconds) return '00:00';
+  const m = Math.floor(seconds / 60);
+  const s = Math.floor(seconds % 60);
+  return `${m < 10 ? '0' : ''}${m}:${s < 10 ? '0' : ''}${s}`;
+}
+  onLoadedMetadata(e:Event) {
+  console.log('e :', e.timeStamp);
+    this.duration = this.audioElement.nativeElement.duration;
+    console.log('    this.duration :',     this.duration);
+  }
+
+  seekTo(event: any) {
+    this.audioElement.nativeElement.currentTime = event.detail.value;
+    this.currentTime = event.detail.value;
+  }
+
 }
