77bff72d553259a542d84e46a6ecc162e9d0f023
nehru00123@gmail.com <nehru00123@gmail.com>
2025-07-10 09:29:56 +0530

    Lord Krishna's page is being polished

--- src/app/krishna-page/krishna-page.page.ts
+++ src/app/krishna-page/krishna-page.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonButtons, IonButton, IonMenuButton, IonBadge, IonSegment, IonSegmentButton, IonLabel, IonFooter } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonButtons, IonButton, IonMenuButton, IonSegment, IonSegmentButton, IonLabel, IonFooter } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { chevronDownOutline, notificationsOutline, optionsOutline, home, heartOutline, cafeOutline, personOutline } from 'ionicons/icons';
+import { chevronDownOutline, notificationsOutline, optionsOutline, home, heartOutline, cafeOutline, personOutline, chevronBack, chevronForward } from 'ionicons/icons';
 import { DisplayCardListComponent, } from '../components/display-card-list/display-card-list.component';
 import { CardItem, InputData } from '../Utils/models';
 import { KrishnaServiceService } from './krishna-service.service';
@@ -14,31 +14,51 @@ import { DataSharingService } from '../services/data-sharing.service';
   templateUrl: './krishna-page.page.html',
   styleUrls: ['./krishna-page.page.scss'],
   standalone: true,
-  imports: [IonFooter, NgFor, IonLabel, IonSegmentButton, IonMenuButton, IonSegment, IonBadge, IonButton, IonButtons, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, DisplayCardListComponent]
+  imports: [IonFooter, NgFor, IonLabel, IonSegmentButton, IonMenuButton, IonSegment, IonButton, IonButtons, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, DisplayCardListComponent]
 })
-export class KrishnaPagePage  {
+export class KrishnaPagePage implements OnInit, OnDestroy {
   languages = [
-  { value: 'Tamil', label: 'Tamil' },
-  { value: 'English', label: 'English' },
-  { value: 'Hindi', label: 'Hindi' },
-  { value: 'Telugu', label: 'Telugu' },
-  { value: 'Malayalam', label: 'Malayalam' },
-  { value: 'Kannada', label: 'Kannada' }
+  { value: 'Arati', label: 'Arati' },
+  { value: 'Hare Krishna Kirtan', label: 'Hare Krishna Kirtan' },
+  { value: 'Stories', label: 'Stories' },
+  { value: 'Philosophy', label: 'Philosophy' },
+  { value: 'Discussion', label: 'Discussion' },
+  { value: 'Images', label: 'Images' }
 ];
-
 
 inputDatas: InputData[] = [];
 
+// Carousel properties
+currentImageIndex = 0;
+carouselInterval: any;
+isCarouselPaused = false;
+readonly carouselImages = [
+  {
+    src: 'https://res.cloudinary.com/dbmkctsda/image/upload/v1751499823/balakrishna_i8eykp.jpg',
+    alt: 'Bala Krishna'
+  },
+  {
+    src: 'https://res.cloudinary.com/dbmkctsda/image/upload/v1752102810/convincing_gopis_ymrmnm.jpg',
+    alt: 'Krishna with Gopis'
+  },
+  {
+    src: 'https://res.cloudinary.com/dbmkctsda/image/upload/v1751941178/astasaki_drhwrf.jpg',
+    alt: 'Astasaki Krishna'
+  },
+  {
+    src: 'https://res.cloudinary.com/dbmkctsda/image/upload/v1751499810/krishnabalaram_iaduro.jpg',
+    alt: 'Astasaki Krishna'
+  }
+];
+
+selectedLang: string = 'Arati';
+
   constructor(private krishnaService: KrishnaServiceService, private router: Router, private dataSharingService: DataSharingService) { 
-    addIcons({home,heartOutline,cafeOutline,personOutline,chevronDownOutline,notificationsOutline,optionsOutline});
+    addIcons({chevronBack,chevronForward,home,heartOutline,cafeOutline,personOutline,chevronDownOutline,notificationsOutline,optionsOutline});
   }
 
-onCardSelected(item: CardItem) {
-  this.dataSharingService.setSelectedCardItem(item);
-  this.router.navigate(['/card-details']);
-}
-
 ngOnInit() {
+  this.startCarousel();
   this.krishnaService.getKrishnaData().subscribe({
     next: (data:InputData[]) => {
       this.inputDatas = data;
@@ -47,6 +67,57 @@ ngOnInit() {
       console.error('Error fetching Krishna data:', error);
     }
   })
+}
+
+ngOnDestroy() {
+  this.stopCarousel();
+}
+
+startCarousel() {
+  this.carouselInterval = setInterval(() => {
+    this.nextImage();
+  }, 5000); // Change image every 5 seconds
+}
+
+stopCarousel() {
+  if (this.carouselInterval) {
+    clearInterval(this.carouselInterval);
+    this.carouselInterval = null;
+  }
+}
+
+nextImage() {
+  this.currentImageIndex = (this.currentImageIndex + 1) % this.carouselImages.length;
+}
+
+previousImage() {
+  this.currentImageIndex = this.currentImageIndex === 0 
+    ? this.carouselImages.length - 1 
+    : this.currentImageIndex - 1;
+}
+
+goToImage(index: number) {
+  this.currentImageIndex = index;
+  // Reset the carousel timer when user manually navigates
+  this.stopCarousel();
+  if (!this.isCarouselPaused) {
+    this.startCarousel();
+  }
+}
+
+pauseCarousel() {
+  this.isCarouselPaused = true;
+  this.stopCarousel();
+}
+
+resumeCarousel() {
+  this.isCarouselPaused = false;
+  this.startCarousel();
+}
+
+onCardSelected(item: CardItem) {
+  this.dataSharingService.setSelectedCardItem(item);
+  this.router.navigate(['/card-details']);
 }
 
 }
